17a8d70b919904433b3f3f82bbc80ad82c725aa4
bradeu <bradeu01@gmail.com>
2025-01-19 10:23:52 -0800

    chore: clean code

---
+++ frontend/src/components/loader/index.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from "react";
+import { words } from "./data";
+
+import "./loader.css"; // Import plain CSS
+import { introAnimation, collapseWords } from "./animation";
+
+const Loader = ({ timeline }: { timeline?: GSAPTimeline }) => {
+  const loaderRef = useRef(null);
+  const progressRef = useRef(null);
+  const progressNumberRef = useRef(null);
+  const wordGroupsRef = useRef(null);
+
+  useEffect(() => {
+    if (timeline) {
+      timeline
+        .add(introAnimation(wordGroupsRef))
+        .add(collapseWords(loaderRef), "-=1");
+    }
+  }, [timeline]);
+
+  return (
+    <div className="loader__wrapper">
+      <div className="loader__progressWrapper">
+        <div className="loader__progress" ref={progressRef}></div>
+        <span className="loader__progressNumber" ref={progressNumberRef}>
+          0
+        </span>
+      </div>
+      <div className="loader" ref={loaderRef}>
+        <div className="loader__words">
+          <div className="loader__overlay"></div>
+          <div ref={wordGroupsRef} className="loader__wordsGroup">
+            {words.map((word, index) => {
+              return (
+                <span key={index} className="loader__word">
+                  {word}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
