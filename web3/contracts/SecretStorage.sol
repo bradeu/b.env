62b22f9388bca05fa3f88c7a8eaea1432f2d1e2d
danee <danethan@students.cs.ubc.ca>
2025-01-18 22:03:14 -0800

    fixed storage.sol

--- web3/contracts/SecretStorage.sol
+++ web3/contracts/SecretStorage.sol
@@ -2,30 +2,38 @@
 pragma solidity ^0.8.9;
 
 contract SecretStorage {
+    // struct
     struct ApiLots {
         address lotAddress;
         string encryptedApiKey;
     }
 
-    mapping(address => ApiLots) public users;
+    mapping(address => ApiLots) public lots;
 
     event EncryptedApiKeyStored(address indexed user, string encryptedApiKey);
 
-    function storeEncryptedApiKey(string memory _encryptedApiKey) public {
+    //function made public
+    function storeEncryptedApiKey(
+        address targetAddress,
+        string memory _encryptedApiKey
+    ) public {
         require(
-            bytes(users[msg.sender].encryptedApiKey).length == 0,
+            bytes(lots[targetAddress].encryptedApiKey).length == 0,
             "API key already stored"
         );
 
-        users[msg.sender] = ApiLots({
-            lotAddress: msg.sender,
+        lots[targetAddress] = ApiLots({
+            lotAddress: targetAddress,
             encryptedApiKey: _encryptedApiKey
         });
 
-        emit EncryptedApiKeyStored(msg.sender, _encryptedApiKey);
+        emit EncryptedApiKeyStored(targetAddress, _encryptedApiKey);
     }
 
-    function getEncryptedApiKey() public view returns (string memory) {
-        return users[msg.sender].encryptedApiKey;
+    // get is made public
+    function getEncryptedApiKeyForAddress(
+        address targetAddress
+    ) public view returns (string memory) {
+        return lots[targetAddress].encryptedApiKey;
     }
 }
